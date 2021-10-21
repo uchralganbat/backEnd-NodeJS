c20a3a804d43b057fbe255c45f6969960fa0c755
Uchral <uchralganbat0220@gmail.com>
2021-10-21 21:59:55 +0800

    bootstrapped the app structure

---
+++ src/utils/connect.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import config from 'config';
+
+async function connect() {
+    const dbUri = config.get<string>("dbUri");
+    try {
+        await mongoose.connect(dbUri);
+        console.log('db has connected');
+    } catch(error) {
+        console.error('Could not connect to db');
+        process.exit(1);
+    }
+}
+
+export default connect;
