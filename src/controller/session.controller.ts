c0774ca650147fa56118d11f2514de05e1ea7162
Uchral <uchralganbat0220@gmail.com>
2021-10-22 16:35:28 +0800

    user and session controller

---
+++ src/controller/session.controller.ts
@@ -0,0 +1,34 @@
+import { Response, Request } from "express";
+import config from "config";
+import { createSession, findSessions } from "../service/session.service";
+import { validatePassword } from "../service/user.service";
+import { signJwt } from "../utils/jwt.utils";
+
+export async function createUserSessionHandler(req: Request, res: Response) {
+    
+    const user = await validatePassword(req.body)
+
+    if(!user) return res.status(401).send('Invalid email or password');
+
+    const session = await createSession(user._id, req.get('user-agent') || "");
+
+    const accessToken = signJwt(
+        { ...user, session: session._id },
+        { expiresIn: config.get('accessTokenTtl')}
+    );
+    
+    const refreshToken = signJwt(
+        { ...user, session: session._id },
+        { expiresIn: config.get('refreshTokenTtl')}
+    );
+    
+    return res.send({ accessToken, refreshToken });
+}
+
+export async function getUserSessionHandler(req: Request, res: Response) {
+    const userId = res.locals.user._id;
+
+    const sessions = await findSessions({user: userId, valid: false});
+
+    return res.send(sessions);
+}
